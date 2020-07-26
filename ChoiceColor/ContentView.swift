7767591cbff5dd4fcc60c82399bb7c8a31aa3371
victor <victoru7@gmail.com>
2020-07-26 14:19:24 +0300

    Initial Commit

---
+++ ChoiceColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChoiceColor
+//
+//  Created by Виктор Устюжанин on 26.07.2020.
+//  Copyright © 2020 Виктор Устюжанин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
